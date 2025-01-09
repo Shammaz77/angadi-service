306185b2fa557e8f05d460fdd2daa06efa9f1659
Athul krishna v <athulkrishnav@Athuls-MacBook-Air.local>
2025-01-09 15:40:41 +0530

    requirements

--- db.py
+++ db.py
@@ -1,9 +1,5 @@
 from pymongo import MongoClient
-from os import getenv, environ
-
-# environ['ATLAS_SRV'] = 'mongodb+srv://athultest:aTT66L87rsKSkh7p@cluster-angadiapp.92nvk.mongodb.net/'
-environ['ATLAS_SRV'] = 'mongodb+srv://angadi:r2vME!-8HSSLhTd@cluster-angadiapp.92nvk.mongodb.net/'
-environ['MONGO_DB'] = 'angadi_app'
+from os import getenv
 
 ATLAS_CONNECTION_STRING = str(getenv('ATLAS_SRV'))
 MONGO_DB = str(getenv('MONGO_DB'))
